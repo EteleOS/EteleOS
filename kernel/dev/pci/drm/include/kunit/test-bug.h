59f532411dab99a991b0d30b64e1c76dd8d1ab2a
tpddl08 <tpddl08@gmail.com>
2026-09-04 10:00:59 +0700

    change plan
    PeteleOS will do not base on OpenBSD 7.9 yet, i have new plan

--- kernel/dev/pci/drm/include/kunit/test-bug.h
+++
@@ -1,8 +0,0 @@
-/* Public domain. */
-
-#ifndef _KUNIT_TEST_BUG_H
-#define _KUNIT_TEST_BUG_H
-
-#define kunit_fail_current_test(fmt, ...)
-
-#endif
--- kernel/dev/pci/drm/include/kunit/visibility.h
+++
@@ -1,8 +0,0 @@
-/* Public domain. */
-
-#ifndef _KUNIT_VISIBILITY_H
-#define _KUNIT_VISIBILITY_H
-
-#define EXPORT_SYMBOL_IF_KUNIT(x)
-
-#endif
